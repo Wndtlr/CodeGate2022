4a97618a3b45dc3a4589065a9141915697d8317d
Daniel Kim <wndtlr@daniels-mbp.colgate.edu>
2022-04-23 13:02:42 -0400

    aaa

--- glasses.py
+++ glasses.py
@@ -1,12 +1,23 @@
-import keyboard
 import pyautogui
 from PIL import Image 
+def run_program(mag_size):
+    toggleZoom = false  
+    toggleTerminate = true
+    #While toggle is on,
+
+    while (toggleTerminate): 
+        if(keyboard.is_pressed("ctrl+shift+m")):
+            toggleZoom = not toggleZoom
+
+        if(toggleZoom):
+            zoom(mag_size, toggleZoom)
+
+        if(keyboard.is_pressed("ctrl+shift+t")):
+            toggleTerminate = false
 
 
-position = pyautogui.position()
-#While toggle is on,
-while (keyboard.is_pressed("ctrl+shift+m")):
-    position = pyautogui.position()
-    
-
-    
+def zoom(mag_size, toggleZoom):
+    while(toggleZoom):
+        position = pyautogui.position()
+        screenFile = open(screenshot.png)
+run_program(mag_size)
