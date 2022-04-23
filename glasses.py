3ae0c44b12b97b5481bb6f7416e3f2a0cd6d729c
Daniel Kim <wndtlr@daniels-mbp.colgate.edu>
2022-04-23 12:16:39 -0400

    minor updates

--- glasses.py
+++ glasses.py
@@ -1,1 +1,12 @@
-#Starting here
+import keyboard
+import pyautogui
+from PIL import Image 
+
+
+position = pyautogui.position()
+#While toggle is on,
+while (keyboard.is_pressed("ctrl+shift+m")):
+    position = pyautogui.position()
+    
+
+    
