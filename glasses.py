61f44104fd5713a8867d442419bd8b942fab13fd
Daniel Kim <wndtlr@daniels-mbp.colgate.edu>
2022-04-23 13:34:07 -0400

    c

--- glasses.py
+++ glasses.py
@@ -4,20 +4,21 @@ def run_program(mag_size):
     toggleZoom = false  
     toggleTerminate = true
     #While toggle is on,
-
     while (toggleTerminate): 
-        if(keyboard.is_pressed("ctrl+shift+m")):
+        if(keyboard.is_pressed("ctrl+shift+m")): #toggle between zooming and not
             toggleZoom = not toggleZoom
 
-        if(toggleZoom):
+        if(toggleZoom): #check to see if it is zooming and then call the function to do so 
             zoom(mag_size, toggleZoom)
 
-        if(keyboard.is_pressed("ctrl+shift+t")):
+        if(keyboard.is_pressed("ctrl+shift+t")): #once the terminate keybind is pressed exit.
             toggleTerminate = false
 
-
 def zoom(mag_size, toggleZoom):
-    while(toggleZoom):
+    screensize = pyautogui.size() #get screensize to compare with the area we are zooming into. 
+
+    while(toggleZoom): #continue to do thi s
         position = pyautogui.position()
         screenFile = open(screenshot.png)
+        
 run_program(mag_size)
